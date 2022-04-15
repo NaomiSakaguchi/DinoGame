184b22d91fa2702491b6fdc4d9f54b2bcbae9095
NaomiSakaguchi <68404372+NaomiSakaguchi@users.noreply.github.com>
2022-04-15 12:12:30 -0300

    game over functionality added. images added

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// variáveis globais
 const dino = document.querySelector('.dino');
+const background = document.querySelector('.background'); 
 let isJumping = false;
+let position = 0;
 
-const background = document.querySelector('.background');   
-
+// função para quando apertar o espaço, o dino pule, caso jã não esteja pulando
 function handleKeyUp(event) {
     if(event.keyCode === 32) {
         if(!isJumping) {
@@ -10,9 +12,8 @@ function handleKeyUp(event) {
         }
     }
 }
-
+ // função para pular
 function jump() {
-    let position = 0;
     isJumping = true;
 
     let upInterval = setInterval(() => {
@@ -37,13 +38,31 @@ function jump() {
     }, 20);
 } 
 
+ // função que cria cactos randomicamente
 function createCactus() {
     const cactus = document.createElement('div');
     let cactusPosition = 1000;
+    let randomTime = Math.random() * 6000; // gera um numero aleatório entre 0 e 1
     
     cactus.classList.add('cactus');
     cactus.style.left = 1000 + 'px';
     background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
+        if(cactusPosition < -60) { // para remover o cacto que sai da tela, para evitar processamento desnecessario
+            clearInterval(leftInterval);
+            background.removeChild(cactus);
+       } else if(cactusPosition > 0 && cactusPosition < 60 && position < 60) { // se o cacto estiver na tela e sua posição for menor que 60, significa que ele está em cima do dinossauro que não deverá estar pulando, ou seja, game over.
+            clearInterval(leftInterval); // o cacto para de ir para esquerda
+            document.body.innerHTML = '<h1 class="game-over">Fim de jogo</h1>';
+        } else {
+            cactusPosition -= 10; // velocidade com que o cacto se move para a esquerda 
+            cactus.style.left = cactusPosition + 'px';
+        }
+    }, 20);
+
+     //recursividade: função que invoca ela mesma dentro dela (espelho)
+    setTimeout(createCactus, randomTime);
 }
 
 createCactus();
